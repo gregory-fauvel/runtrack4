12192eb895e5c33a0ba8732e17d47335a46de93b
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-05-06 18:00:46 +0200

    jour3 job01 FINI compris avec du mal

---
+++ jour3/job01/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<title>jour3job01</title>
+</head>
+<body style="background-color: #EDEDED">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">LPTF</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Units</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jobs</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Skills</a>
+      </li>  
+    </ul>
+  </div>
+</nav>
+
+	<nav class="navbar navbar-light">
+  		<span class="mx-auto h1">LaPlateforme_</span>
+	</nav>
+
+
+	<article class="d-flex justify-content-around">
+		<div class="row d-flex justify-content-around ">
+
+			<div class="row col-2" style="background-color: white " >
+				<figure class="figure">
+  				<img width="460" src="images/papillon.jpg" class="figure-img img-fluid rounded" alt="...">
+  				<figcaption class="figure-caption"> <span class="mx-auto h3">Un papillon</span><br><p>Un papilllon c'est comme une chenille<br> mais avec des ailes.Ne pas ingerer</p></figcaption>
+  				<button type="button" class="btn btn-primary">Commandez votre propre papillon</button>
+				</figure>
+			</div>
+
+		<div class="col-6" style="background-color: #AFAFAF">
+			<span class="mx-auto h2">Bonjour,monde!</span>
+			<br>
+			<br>
+			<p>Il existe plusieurs visions du termes:<br>Le monde est la matière,l'espace et les phénomènes qui nous sont accessibles par les sens ,l'expérience ou la raison.<br>Le sens le plus courant désigne notre planète , la Terre,avec ses habitants ,et son environnement plus ou moins naturel.Le sens étendu désigne l'univer dans son ensemble.</p>
+			<br>
+			<button type="button" class="btn btn-danger">Rebooter le monde</button><br>
+			<nav aria-label="Page navigation example"><br><br>
+  			<ul class="pagination">
+			    <li class="page-item"><a class="page-link" href="#"><<</a></li>
+			    <li class="page-item"><a class="page-link" href="#">1</a></li>
+			    <li class="page-item"><a class="page-link" href="#">2</a></li>
+			    <li class="page-item"><a class="page-link" href="#">3</a></li>
+			    <li class="page-item"><a class="page-link" href="#">>></a></li>
+  			</ul>
+			</nav>
+		</div>
+			
+		
+				<div class="btn-group-vertical bg-white  ">
+		  			<button type="button" class="text-blue btn btn-outline-primary">Limbes</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Gourmandises</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Avarice</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Colère</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Hérésie</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">violence</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Ruses et tromperies</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Trahison</button>
+		  			<button type="button" class="text-blue btn btn-outline-primary">Internet Explorer</button>
+				</div>
+		</div>
+
+
+	</article><br>
+
+	<div class="row">
+		<div class="w-100 d-flex align-items-center flex-column">
+		<div  class="w-50 d-flex justify-content-center">
+			<p>Installation de AI 9000</p>
+		</div>
+		<div class="progress w-50">
+			<div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+	</div>
+	</div>
+	<br>
+	<br>
+
+	<article class="d-flex justify-content-around" >
+		<section class="flex-column">
+			<h5>Recevez votre copie gratuite d'internet 2!</h5>
+			<form>
+				<div class="input-group col-12 align-items-center">
+					<div class="input-group-prepend">
+					    <span class="input-group-text" id="basic-addon1">@</span>
+					 </div>
+					 <input type="text" class="form-control" placeholder="Login" aria-label="Login" aria-describedby="basic-addon1">
+				</div>
+				<br>
+
+					<div class="input-group col-12">
+					  <input type="text" class="form-control" placeholder="Mot de passe" aria-label="Recipient's username" aria-describedby="basic-addon2">
+					  <div class="input-group-append">
+					    <span class="input-group-text" id="basic-addon2">@example.com</span>
+					  </div>
+					</div>
+				<br>
+					<label for="basic-url">URL des internets 2 et 2.1 beta</label>
+				<br>
+					<div class="input-group col-12">
+					  <div class="input-group-prepend ">
+					    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+					  </div>
+					  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+					</div>
+				<br>
+					<div class="input-group col-12">
+					  <div class="input-group-prepend">
+					    <span class="input-group-text">DogeCoin</span>
+					  </div>
+					  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+					  <div class="input-group-append">
+					    <span class="input-group-text">.00</span>
+					  </div>
+					</div>
+			</section>
+
+			<section class="flex-column col-3">
+					  <div class="form-group row flex-column">
+					    <label for="inputEmail3" class="col-2 col-form-label">Email</label>
+					    <div class="col-sm-10">
+					      <input type="email" class="form-control " id="inputEmail3">
+					    </div>
+					  </div>
+					  <div class="form-group row flex-column">
+					    <label for="inputPassword3" class="col-2 col-form-label">Password</label>
+					    <div class="col-sm-10">
+					      <input type="password" class="form-control" id="inputPassword3">
+					    </div>
+					  </div>
+					  <div class="form-group row">
+					    <div class="col-sm-2"></div>
+					    <div class="col-sm-10">
+					      <div class="form-check">
+					        <input class="form-check-input" type="checkbox" id="gridCheck1">
+					        <label class="form-check-label" for="gridCheck1">
+					          Check me out
+					        </label>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="form-group row">
+					    <div class="col-sm-10">
+					      <button type="submit" class="btn btn-primary">Submit</button>
+					    </div>
+					  </div>
+					</form>
+			</div>
+			</section>
+	</article>
+
+		
+
+
+
+
+	
+
+</body>
+</html>
